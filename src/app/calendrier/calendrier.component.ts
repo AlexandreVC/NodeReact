0b3a34e3d0c38b86c12d0c183bfaeae873327cee
AVC <151655878+AlexandreVC@users.noreply.github.com>
2023-12-31 20:57:45 +0100

    Update calendrier.component.ts

--- src/app/calendrier/calendrier.component.ts
+++ src/app/calendrier/calendrier.component.ts
@@ -11,6 +11,23 @@ export class CalendrierComponent {
   viewDate: Date = new Date();
   events = [];
   view: CalendarView = CalendarView.Month;
+
+  // Updated properties for current month and day
+  currentMonth: string = this.viewDate.toLocaleString('fr-FR', { month: 'long' });
+  currentDay: string = this.viewDate.toLocaleString('fr-FR', { weekday: 'long', day: 'numeric' });
+
+
+  constructor() {
+    // Set initial values for currentMonth and currentDay
+    this.setCurrentMonthAndDay();
+  }
+
+  setCurrentMonthAndDay(): void {
+    // You can use date-fns format function or Angular date pipe in the template
+    this.currentMonth = this.viewDate.toLocaleString('fr-FR', { month: 'long' });
+    this.currentDay = this.viewDate.toLocaleString('fr-FR', { weekday: 'long', day: 'numeric' });
+  }
+
   previousMonth(): void {
     this.viewDate = subMonths(this.viewDate, 1);
   }
