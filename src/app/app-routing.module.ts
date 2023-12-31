c03865747d7d4a5fbcc9b3ab0f644c5903b10d5c
Alex Garnier <alexandre.garnier@edu.devinci.fr>
2023-12-31 19:57:39 +0100

    front sans superflu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LessonListPageComponent} from "./lesson-list-page/lesson-list-page.component";
-import {LessonEditFormComponent} from "./lesson-edit-form/lesson-edit-form.component";
-import {LessonDetailPageComponent} from "./lesson-detail-page/lesson-detail-page.component";
 import {AccueilComponent} from "./accueil/accueil.component";
 import {ResultatsNotesComponent} from "./resultats-notes/resultats-notes.component";
 import {CalendrierComponent} from "./calendrier/calendrier.component";
 import {ListeCoursComponent} from "./liste-cours/liste-cours.component";
 
 const routes: Routes = [
-  { path:'lesson-edit-form', component: LessonEditFormComponent },
-  { path:'lesson-list', component: LessonListPageComponent },
-  { path:'lesson/:id', component: LessonDetailPageComponent },
   { path: 'Accueil', component: AccueilComponent },
   { path: 'resultats', component: ResultatsNotesComponent },
   { path: 'calendrier', component: CalendrierComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LessonEditFormComponent } from './lesson-edit-form/lesson-edit-form.component';
-import { LessonSearchPageComponent } from './lesson-search-page/lesson-search-page.component';
-import { MenuNavBarComponent } from './menu-nav-bar/menu-nav-bar.component';
-import { LessonListPageComponent } from './lesson-list-page/lesson-list-page.component';
-import { LessonDetailPageComponent } from './lesson-detail-page/lesson-detail-page.component';
 import {HttpClientModule} from "@angular/common/http";
 import { ResultatsNotesComponent } from './resultats-notes/resultats-notes.component';
 import { CalendrierComponent } from './calendrier/calendrier.component';
@@ -20,11 +15,6 @@ import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 @NgModule({
   declarations: [
     AppComponent,
-    LessonEditFormComponent,
-    LessonSearchPageComponent,
-    MenuNavBarComponent,
-    LessonListPageComponent,
-    LessonDetailPageComponent,
     ResultatsNotesComponent,
     CalendrierComponent,
     ListeCoursComponent,
